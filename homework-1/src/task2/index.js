d3b4a415fed6e15929770efe66daa1b61131ee05
Phu Le <phu_le@epam.com>
2021-09-21 19:46:14 +0700

    task2: add error handling

--- homework-1/src/task2/index.js
+++ homework-1/src/task2/index.js
@@ -5,8 +5,14 @@ const path = __dirname;
 const csvFilePath = `src/csv/nodejs-hw1-ex1.csv`;
 
 const readStream = fs.createReadStream(csvFilePath);
-
 const writeStream = fs.createWriteStream(`${path}/jsonData.txt`);
+
+readStream.on("error", (error) => {
+  console.error(error);
+})
+writeStream.on("error", (error) => {
+  console.error(error);
+})
 
 readStream.pipe(csv()).pipe(writeStream);
 
--- homework-1/src/task3/build/task2/index.js
+++ homework-1/src/task3/build/task2/index.js
@@ -8,4 +8,10 @@ var path = __dirname;
 var csvFilePath = "src/csv/nodejs-hw1-ex1.csv";
 var readStream = fs.createReadStream(csvFilePath);
 var writeStream = fs.createWriteStream("".concat(path, "/jsonData.txt"));
+readStream.on("error", function (error) {
+  console.error(error);
+});
+writeStream.on("error", function (error) {
+  console.error(error);
+});
 readStream.pipe(csv()).pipe(writeStream);
