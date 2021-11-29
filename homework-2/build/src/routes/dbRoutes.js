8871ae6a89ff29b447e837428208889dad979c14
Phu Le <phu_le@epam.com>
2021-11-29 18:18:33 +0700

    homework-5: add preferences to .gitignore

--- homework-2/build/src/routes/dbRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dbControllers_1 = require("../controllers/dbControllers");
-const logger_1 = require("../middlewares/logger");
-const dbRouter = (0, express_1.Router)();
-// reset database
-dbRouter.post('/reset', logger_1.loggerMiddleware, dbControllers_1.resetDatabaseController);
-exports.default = dbRouter;
